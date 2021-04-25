f9cf9cbce7b04196595f898c810a15f730fefaf4
majorojas09 <majorooc09@gmail.com>
2021-04-25 18:50:05 +0000

    FINAL

---
+++ src/js/component/TrafficLight.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './index.scss'
+
+export default class TrafficLight extends React.Component{
+    constructor() {
+		super();
+		this.state = { clickedLight: null };
+	}
+    render(){
+        let redClass = '';
+        if (this.state.clickedLight == 'red') redClass = 'selected';
+		let yellowClass = '';
+        if (this.state.clickedLight == 'yellow') yellowClass = 'selected';
+        let greenClass = '';
+        if (this.state.clickedLight == 'green') greenClass = 'selected';
+        return (
+		<div id="semaforo">
+			<div className={"red light" + redClass} onClick = {() => this.setState({ clickedLight: 'red' })}></div>
+            <div className={"yellow light" + redClass} onClick = {() => this.setState({ clickedLight: 'yellow' })}></div>
+            <div className={"green light" + redClass} onClick = {() => this.setState({ clickedLight: 'green' })}></div>
+		</div>
+    );
+    }
+}
